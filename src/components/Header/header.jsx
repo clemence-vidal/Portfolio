e003439046670a28b47a3a7319b6fb7609d8f8f6
Clemence Vidal <clemencevidal0512@gmail.com>
2024-03-14 18:23:50 +0100

    ajout d'un menu burger pour la version mobile, petits changements graphiques pour la version mobile

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,25 +2,38 @@ import React, { useState } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import "./header.scss"
 import logo from "../../assets/logo-portfolio.png"
-import menuIcon from "../../assets/menu-icon.svg";
 
 
 function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [showLinks, setShowLinks] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
+    const handleShowLinks = () => {
+        setShowLinks(!showLinks);
     };
 
     return (
-        <nav className="navbar">
-            <div className="liens">
-                <Link to="home#a-propos">A propos</Link>
-                <Link to="home#projets">Projets</Link>
-                <Link to="home#formation">Formation</Link>
-                <Link to="home#skills">Compétences</Link>
-                <Link to="home#contact">Contact</Link>
-            </div>
+        <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+            <div className="logo">Logo</div>
+            <ul className="liens">
+                <li className="liens-item">
+                    <Link to="home#a-propos">A propos</Link>
+                </li>
+                <li className="liens-item">
+                    <Link to="home#projets">Projets</Link>
+                </li>
+                <li className="liens-item">
+                    <Link to="home#formation">Formation</Link>                    
+                </li>
+                <li className="liens-item">
+                    <Link to="home#skills">Compétences</Link>                    
+                </li>
+                <li className="liens-item">
+                    <Link to="home#contact">Contact</Link>                    
+                </li>
+            </ul>
+            <button className="burger" onClick={handleShowLinks}>
+                <span className="burger-bar"></span>
+            </button>
         </nav>
     );
 }
